75c1f0b931271b065cc0c5f06e53137862b9c25f
Olderestin <sdordiuk@gmail.com>
2024-02-02 18:41:34 +0200

    :sparkles: feat: Add models to admin panel

--- team/admin.py
+++ team/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from team.models import Member, Team
+
 # Register your models here.
+class TeamMemberInline(admin.TabularInline):
+    model = Team.members.through
+    extra = 1
+    fields = ['member']
+
+class MemberTeamInline(admin.TabularInline):
+    model = Member.teams.through
+    extra = 1
+    fields = ['team']
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    inlines = [TeamMemberInline]
+    exclude = ['members']
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+    inlines = [MemberTeamInline]
+
--- team/models.py
+++ team/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 
 class Team(models.Model):
     title = models.CharField(max_length=50)
