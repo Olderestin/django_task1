ede4035253837d04fa0317896141ab9834423ba4
Olderestin <sdordiuk@gmail.com>
2024-02-21 18:35:18 +0200

    :white_check_mark: test: Update tests

--- team/tests/member_tests.py
+++ team/tests/member_tests.py
@@ -1,106 +1,71 @@
 import pytest
-from rest_framework.test import APIClient
-from django.urls import reverse
 from team.models import Member
+from team.tests.fixtures import client, member_data
 
 #positive case
 @pytest.mark.django_db
-def test_create_member():
-    client = APIClient()
-    
-    data = {
-        'first_name': 'John',
-        'last_name': 'Doe',
-        'email': 'john@example.com'
-    }
-    
-    response = client.post('/api/member/', data)
-    assert response.status_code == 201
-    assert response.data['email'] == "john@example.com"
-    assert Member.objects.filter(email="john@example.com").exists()
+def test_create_member(client: client, member_data: member_data) -> None:    
+    for data in member_data:
+        response = client.post('/api/member/', data)
+        assert response.status_code == 201
+        assert response.data['email'] == data['email']
+        assert Member.objects.filter(email=data['email']).exists()
 
 @pytest.mark.django_db
-def test_get_members():
-    client = APIClient()
-    
+def test_get_members(client:client) -> None:
     response = client.get('/api/team/')
     assert response.status_code == 200
     assert len(response.data) == Member.objects.count()
 
 @pytest.mark.django_db
-def test_retrieve_member():
-    member = Member.objects.create(first_name='Jane', last_name='Doe', email='jane@example.com')
+def test_retrieve_member(client: client, member_data: member_data) -> None:
 
-    client = APIClient()
-    
-    response = client.get(f'/api/member/{member.id}/')
-    assert response.status_code == 200
-    assert response.data['email'] == "jane@example.com"
+    for data in member_data:
+        member = Member.objects.create(first_name=data['first_name'], last_name=data['last_name'], email=data['email'])
 
-@pytest.mark.django_db
-def test_update_member():
-    member = Member.objects.create(first_name='Jane', last_name='Doe', email='jane@example.com')
-    
-    client = APIClient()
-    
-    data = {
-        'first_name': 'Updated',
-        'last_name': 'Updated',
-        'email': 'jane_updated@example.com'
-    }
-    
-    response = client.put(f'/api/member/{member.id}/', data)
-    assert response.status_code == 200
-    assert Member.objects.get(id=member.id).email == "jane_updated@example.com"
+        response = client.get(f'/api/member/{member.id}/')
+        assert response.status_code == 200
+        assert response.data['email'] == data['email']
 
 @pytest.mark.django_db
-def test_delete_member():
+def test_update_member(client: client, member_data: member_data) -> None:
     member = Member.objects.create(first_name='Jane', last_name='Doe', email='jane@example.com')
+    
+    for data in member_data:
+        response = client.put(f'/api/member/{member.id}/', data)
+        assert response.status_code == 200
+        assert Member.objects.get(id=member.id).email == data['email']
 
-    client = APIClient()
+@pytest.mark.django_db
+def test_delete_member(client: client, member_data: member_data) -> None:
+    for data in member_data:
+        member = Member.objects.create(first_name=data['first_name'], last_name=data['last_name'], email=data['email'])
 
-    response = client.delete(f'/api/member/{member.id}/')
-    assert response.status_code == 204
-    assert not Member.objects.filter(id=member.id).exists()
+        response = client.delete(f'/api/member/{member.id}/')
+        assert response.status_code == 204
+        assert not Member.objects.filter(id=member.id).exists()
 
 #negative case
 @pytest.mark.django_db
-def test_create_member_missing_required_fields():
-    client = APIClient()
-    
-    data = {
-        'first_name': 'John',
-        'last_name': 'Updated'
-        }
-    
-    response = client.post('/api/member/', data=data)
-    assert response.status_code == 400
-    assert 'email' in response.data
-    assert not Member.objects.exists()
+def test_create_member_missing_required_fields(client: client, member_data: member_data) -> None:
+    for data in member_data:
+        response = client.post('/api/member/', data={k: data[k] for k in list(data.keys())[:2]})
+        assert response.status_code == 400
+        assert 'email' in response.data
+        assert not Member.objects.exists()
 
 @pytest.mark.django_db
-def test_get_nonexistent_member_detail():
-    client = APIClient()
-    
+def test_get_nonexistent_member_detail(client: client) -> None:
     response = client.get('/api/team/999/')
     assert response.status_code == 404
 
 @pytest.mark.django_db
-def test_update_nonexistent_member():
-    client = APIClient()
-    
-    data = {
-        'first_name': 'John',
-        'last_name': 'Doe',
-        'email': 'john@example.com'
-        }
-    
-    response = client.put('/api/team/999/', data=data)
-    assert response.status_code == 404
+def test_update_nonexistent_member(client: client, member_data: member_data) -> None:
+    for data in member_data:
+        response = client.put('/api/team/999/', data=data)
+        assert response.status_code == 404
 
 @pytest.mark.django_db
-def test_delete_nonexistent_member():
-    client = APIClient()
-    
+def test_delete_nonexistent_member(client: client) -> None:
     response = client.delete('/api/team/999/')
     assert response.status_code == 404
--- team/tests/team_tests.py
+++ team/tests/team_tests.py
@@ -1,96 +1,71 @@
 import pytest
 from rest_framework.test import APIClient
 from team.models import Team
+from team.tests.fixtures import client, team_data
 
 #positive case
 @pytest.mark.django_db
-def test_create_team():
-    client = APIClient()
-    data = {
-        "title": "Test Team",
-        "description": "Test Description"
-        }
-    response = client.post('/api/team/', data=data)
-    assert response.status_code == 201
-    assert response.data['title'] == "Test Team"
-    assert Team.objects.filter(title="Test Team").exists()
+def test_create_team(client: client, team_data: team_data) -> None:
+    for data in team_data:
+        response = client.post('/api/team/', data=data)
+        assert response.status_code == 201
+        assert response.data['title'] == data['title']
+        assert Team.objects.filter(title=data['title']).exists()
 
 @pytest.mark.django_db
-def test_get_teams():
-    client = APIClient()
+def test_get_teams(client:client) -> None:
     response = client.get('/api/team/')
     assert response.status_code == 200
     assert len(response.data) == Team.objects.count()
 
 @pytest.mark.django_db
-def test_retrieve_team():
-    team = Team.objects.create(title="Test Team", description="Test Description")
-    client = APIClient()
-    response = client.get(f'/api/team/{team.id}/')
-    assert response.status_code == 200
-    assert response.data['title'] == "Test Team"
+def test_retrieve_team(client: client, team_data: team_data) -> None:
+    for data in team_data:
+        team = Team.objects.create(title=data['title'], description=data['description'])
+        response = client.get(f'/api/team/{team.id}/')
+        assert response.status_code == 200
+        assert response.data['title'] == data['title']
 
 @pytest.mark.django_db
-def test_update_team():
+def test_update_team(client: client, team_data: team_data) -> None:
     team = Team.objects.create(title="Test Team", description="Test Description")
     
-    client = APIClient()
-    
-    data = {
-        "title": "Updated Team",
-        "description": "Updated Description"
-        }
-    
-    response = client.put(f'/api/team/{team.id}/', data=data)
-    assert response.status_code == 200
-    assert Team.objects.get(id=team.id).title == "Updated Team"
+    for data in team_data:
+        response = client.put(f'/api/team/{team.id}/', data=data)
+        assert response.status_code == 200
+        assert Team.objects.get(id=team.id).title == data['title']
 
 @pytest.mark.django_db
-def test_delete_team():   
-    team = Team.objects.create(title="Test Team", description="Test Description")
-    
-    client = APIClient()
-    
-    response = client.delete(f'/api/team/{team.id}/')
-    assert response.status_code == 204
-    assert not Team.objects.filter(id=team.id).exists()
+def test_delete_team(client: client, team_data: team_data) -> None:   
+    for data in team_data:
+        team = Team.objects.create(title=data['title'], description=data['description'])
+
+        response = client.delete(f'/api/team/{team.id}/')
+        assert response.status_code == 204
+        assert not Team.objects.filter(id=team.id).exists()
 
 #negative case
 @pytest.mark.django_db
-def test_create_team_missing_required_fields():
-    client = APIClient()
-    
-    data = {
-        "description": "Test Description"
-        }
-    
-    response = client.post('/api/team/', data=data)
-    assert response.status_code == 400
-    assert 'title' in response.data
-    assert not Team.objects.exists()
+def test_create_team_missing_required_fields(client: client, team_data: team_data) -> None:
+    for data in team_data:    
+        response = client.post('/api/team/', data={k: data[k] for k in list(data.keys())[-1:]})
+        assert response.status_code == 400
+        assert 'title' in response.data
+        assert not Team.objects.exists()
 
 @pytest.mark.django_db
-def test_get_nonexistent_team_detail():
-    client = APIClient()
+def test_get_nonexistent_team_detail(client: client) -> None:
     
     response = client.get('/api/team/999/')
     assert response.status_code == 404
 
 @pytest.mark.django_db
-def test_update_nonexistent_team():
-    client = APIClient()
-    
-    data = {
-        "title": "Updated Team",
-        "description": "Updated Description"
-        }
-    
-    response = client.put('/api/team/999/', data=data)
-    assert response.status_code == 404
+def test_update_nonexistent_team(client: client, team_data: team_data) -> None:
+    for data in team_data:    
+        response = client.put('/api/team/999/', data=data)
+        assert response.status_code == 404
 
 @pytest.mark.django_db
-def test_delete_nonexistent_team():
-    client = APIClient()
-    
+def test_delete_nonexistent_team(client: client) -> None:    
     response = client.delete('/api/team/999/')
     assert response.status_code == 404
